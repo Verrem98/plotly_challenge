3905515190d79ab90a80d69a99135b84370ec88e
bwilkens <bwilkens@users.noreply.github.com>
2023-04-03 10:58:18 +0200

    Initial commit

--- main.py
+++ main.py
@@ -1,16 +1,89 @@
-# This is a sample Python script.
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# opzet voor de multi-plot
+specs = [[{'type': 'domain'}, {'type': 'scatter'}], [{'type': 'scatter'}, {'type': 'scatter'}]]
+titles = ["Peilmomenten", "Team", "Kennis", "Gilde"]  # , "Peilmomenten"
+fig = make_subplots(rows=2, cols=2, subplot_titles=titles, specs=specs)
+
+# drie basis plots,
+fig.add_trace(go.Scatter(x=[0,1,2,3], y=[0,1,2,3]), row=2, col=1)
+fig.add_trace(go.Scatter(x=[0,1,2,3], y=[0,1,2,3]), row=1, col=2)
+fig.add_trace(go.Scatter(x=[0,1,2,3], y=[0,1,2,3]), row=2, col=2)
+
+# plotting de gauge
+plot_bgcolor = "#fff"
+quadrant_colors = [plot_bgcolor, "#2bad4e", "#85e043", "#f2a529", "#f25829"]
+quadrant_text = ["", "<b>Hoge</b>", "<b>Voldoende</b>", "<b>Onvoldoende</b>", "<b>Geen</b>"]
+n_quadrants = len(quadrant_colors) - 1
+current_value = 1
+min_value = 0
+max_value = 3
+hand_length = np.sqrt(2) / 3
+hand_angle = np.pi * (1 - (max(min_value, min(max_value, current_value)) - min_value) / (max_value - min_value))
+l_values = [0.5] + (np.ones(n_quadrants) / 2 / n_quadrants).tolist()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+data = go.Pie(
+    values=l_values,
+    rotation=90,
+    hole=0.75,
+    labels=None,
+    marker_colors=quadrant_colors,
+    text=quadrant_text,
+    name="Gauge",
+    type="pie",
+    # direction="clockwise",
+    sort=False,
+    showlegend=False,
+    hoverinfo="none",
+    textinfo="none",
+    textposition="outside"
+)
 
+fig.add_trace(data, 1, 1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm en Berend')
+shapes = [
+    go.layout.Shape(
+        type="circle", xref="x", yref="y",
+        x0=0.45, x1=0.55,
+        y0=0.45, y1=0.55,
+        fillcolor="#333",
+        line_color="#333"),
+    go.layout.Shape(
+        type="line", xref="x", yref="y",
+        x0=0, x1=1 + hand_length * np.cos(hand_angle),
+        y0=0, y1=1 + hand_length * np.sin(hand_angle),
+        line=dict(color="#333", width=10)
+    )
+]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+fig.update_layout(
+    shapes=shapes)
+
+#layout werkt mischien ook goed?
+layout = go.Layout(
+    showlegend=False,
+    margin=dict(b=0, t=0, l=0, r=0),
+    width=300,
+    height=300,
+    paper_bgcolor=None,
+    shapes=[
+        go.layout.Shape(
+            type="circle", xref="x", yref="y",
+            x0=0.45, x1=0.55,
+            y0=0.45, y1=0.55,
+            fillcolor="#333",
+            line_color="#333",
+        ),
+        go.layout.Shape(
+            type="line", xref="x", yref="y",
+            x0=0.5, x1=0.5 + hand_length * np.cos(hand_angle),
+            y0=0.5, y1=0.5 + hand_length * np.sin(hand_angle),
+            line=dict(color="#333", width=10)
+        )
+    ]
+)
+
+fig.show()
